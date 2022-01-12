5a412d4063794040bbab41100fd4048c8e2710a9
Gie-sta <70339208+Gie-sta@users.noreply.github.com>
2022-01-12 08:17:47 +0200

    formated with prettier

--- components/MovieCard.js
+++ components/MovieCard.js
@@ -1,42 +1,41 @@
-import React from 'react'
-import {Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import React from "react";
+import { Text, TouchableOpacity, Image, StyleSheet } from "react-native";
 
-import { IMAGE_URL } from '../api/constants';
-import { secondaryColor, lightColor } from '../helpers/colors';
+import { IMAGE_URL } from "../api/constants";
+import { secondaryColor, lightColor } from "../helpers/colors";
 
-const MovieCard = ({title, imgUrl, onPress}) => {
+const MovieCard = ({ title, imgUrl, onPress }) => {
   return (
     <TouchableOpacity onPress={onPress} style={styles.card}>
-       <Image 
-       style={styles.image}
-       source={{
-         uri: `${IMAGE_URL}${imgUrl}`,
+      <Image
+        style={styles.image}
+        source={{
+          uri: `${IMAGE_URL}${imgUrl}`,
         }}
-       />
-        <Text style={styles.title}>{title}</Text>
+      />
+      <Text style={styles.title}>{title}</Text>
     </TouchableOpacity>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  card:{
+  card: {
     backgroundColor: secondaryColor,
-    height:'100%',
+    height: "100%",
     paddingTop: 10,
-width:144,
-marginRight: 10
+    width: 144,
+    marginRight: 10,
   },
   title: {
     marginLeft: 4,
     width: 144,
     paddingBottom: 5,
-    color: lightColor
+    color: lightColor,
   },
   image: {
-height:204,
-resizeMode:'contain'
+    height: 204,
+    resizeMode: "contain",
+  },
+});
 
-  }
-})
-
-export default MovieCard
+export default MovieCard;
--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -1,104 +1,112 @@
-import React,{useState,} from 'react'
-import {StyleSheet, Image, Text, View, ScrollView, Dimensions } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Image,
+  Text,
+  View,
+  ScrollView,
+  Dimensions,
+} from "react-native";
 
-import { IMAGE_URL } from '../api/constants';
-import Button from '../components/Button';
-import MoviesList from '../components/MoviesList';
-import { fethchRecomended } from '../api/requests';
-import { primaryColor, secondaryColor, lightColor } from '../helpers/colors';
+import { IMAGE_URL } from "../api/constants";
+import Button from "../components/Button";
+import MoviesList from "../components/MoviesList";
+import { fethchRecomended } from "../api/requests";
+import { primaryColor, secondaryColor, lightColor } from "../helpers/colors";
 
 const DetailsScreen = (props) => {
-
   const movie = props.route.params;
- 
+
   return (
     <ScrollView style={styles.scrollContainer}>
       <View style={styles.movieContainer}>
         <Image
-        style={styles.image}
-        source={{
-          uri: `${IMAGE_URL}${movie.imgUrl}`,
-        }}/>
+          style={styles.image}
+          source={{
+            uri: `${IMAGE_URL}${movie.imgUrl}`,
+          }}
+        />
         <Text style={styles.details}>Movie Details</Text>
-        <Text style={styles.title}><Text style={styles.titleName}> Title: </Text>{movie.title}</Text>
-        <Text style={styles.title}><Text style={styles.titleName}> Release date: </Text>{movie.date}</Text>
+        <Text style={styles.title}>
+          <Text style={styles.titleName}> Title: </Text>
+          {movie.title}
+        </Text>
+        <Text style={styles.title}>
+          <Text style={styles.titleName}> Release date: </Text>
+          {movie.date}
+        </Text>
         <Text style={styles.overview}>{movie.overview}</Text>
-
       </View>
       <View style={styles.btnContiner}>
         <Button
-            text='Play Movie'
-            onPress={() =>
-            props.navigation.navigate('Player', {title: movie.title, id:movie.id}
-            )}
-          />
-        <Button
-            text='Add To Library'/>
+          text="Play Movie"
+          onPress={() =>
+            props.navigation.navigate("Player", {
+              title: movie.title,
+              id: movie.id,
+            })
+          }
+        />
+        <Button text="Add To Library" />
       </View>
       <View style={styles.listContainer}>
-        <MoviesList 
-        id={movie.id}
-        navigation={props.navigation}
-        categoryTitle={'Recomended Similar Movies'}
-        category={fethchRecomended(movie.id)}/>
+        <MoviesList
+          id={movie.id}
+          navigation={props.navigation}
+          categoryTitle={"Recomended Similar Movies"}
+          category={fethchRecomended(movie.id)}
+        />
       </View>
-    
     </ScrollView>
-   
-   
-  )
-}
+  );
+};
 const deviceWidth = Dimensions.get("window").width;
 const deviceHeight = Dimensions.get("window").height;
 
 const styles = StyleSheet.create({
-  scrollContainer:{
+  scrollContainer: {
     padding: 15,
-    backgroundColor: primaryColor
-    },
+    backgroundColor: primaryColor,
+  },
   movieContainer: {
     padding: 5,
     backgroundColor: secondaryColor,
-    },
+  },
   image: {
     width: (deviceWidth * 95) / 100,
     height: (deviceHeight * 40) / 100,
     marginBottom: 10,
-    resizeMode: 'contain'
+    resizeMode: "contain",
   },
-  details:{
-    fontWeight: 'bold',
-    fontSize:25,
+  details: {
+    fontWeight: "bold",
+    fontSize: 25,
     color: lightColor,
-    paddingLeft:4
-
+    paddingLeft: 4,
   },
-  title:{
-    color: lightColor
-
+  title: {
+    color: lightColor,
   },
   titleName: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
     color: lightColor,
-    letterSpacing:1.2
-
+    letterSpacing: 1.2,
   },
-  overview:{
-    textAlign: 'justify',
-    marginLeft:4,
-    marginRight:4,
-    color: lightColor
-
+  overview: {
+    textAlign: "justify",
+    marginLeft: 4,
+    marginRight: 4,
+    color: lightColor,
   },
-  btnContiner:{
+  btnContiner: {
     marginBottom: 20,
     marginTop: 20,
-    marginLeft:4
+    marginLeft: 4,
   },
-  listContainer:{
+  listContainer: {
     minHeight: 280,
-    marginBottom: 30
-  }
-})
+    marginBottom: 30,
+  },
+});
 
-export default DetailsScreen
+export default DetailsScreen;
